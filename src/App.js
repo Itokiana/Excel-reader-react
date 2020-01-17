b23d0960b67b992b4b5528f8572a1b633948d6c9
itokiana <itokiana@gmail.com>
2020-01-17 15:29:17 +0300

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {OutTable, ExcelRenderer} from 'react-excel-renderer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      cols: null,
+      rows: null,
+      rows2: null,
+      search: ""
+    }
+  }
+  fileHandler = (event) => {
+    let fileObj = event.target.files[0];
+
+    //just pass the fileObj as parameter
+    ExcelRenderer(fileObj, (err, resp) => {
+      if(err){
+        console.log(err);            
+      }
+      else{
+        this.setState({
+          cols: resp.cols,
+          rows: resp.rows
+        });
+        let t = "a-zA-Z0-9";
+        console.log({
+          ...this.state,
+          rows: this.state.rows.filter((row) => RegExp('['+ t +']+').test(row[1]) )
+        })
+      }
+    });
+  }
+
+  filterInputHandler = e => {
+    e.preventDefault()
+    this.setState({ search: e.target.value })
+    // console.log(this.state.search)
+    console.log(e.target.value)
+    this.setState({
+      rows2: this.state.rows.filter((row) => RegExp('['+ e.target.value +']+').test(row[1]) )
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Test excel</h1>
+        <input type="file" onChange={this.fileHandler.bind(this)} style={{"padding":"10px"}} />
+        <input type="text" className="form-control" onChange={ this.filterInputHandler } />
+        {
+          this.state.rows2 === null ? (
+            <div>
+              {
+                this.state.rows !== null && this.state.cols !== null ? (
+                  <OutTable data={this.state.rows} columns={this.state.cols} tableClassName="table table-bordered" tableHeaderRowClass="heading" />
+                ) : (<span></span>)
+              }
+            </div>
+          ) : (
+            <div>
+              {
+                <OutTable data={this.state.rows2} columns={this.state.cols} tableClassName="table table-bordered" tableHeaderRowClass="heading" />
+              }
+            </div>
+          )
+        }
+        
+      </div>
+    );
+  }
 }
 
 export default App;
